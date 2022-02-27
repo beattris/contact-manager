b498f40d33bc784cdcb54c349ff713329e8cca35
Beattris <beatriceebirim@gmail.com>
2022-02-27 13:58:16 +0100

    added routes for the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import ContactList from './components/Contacts/ContactList/ContactList';
+import ViewContact from './components/Contacts/ViewContact/ViewContact';
+import EditContact from './components/Contacts/EditContact/EditContact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h2>App component</h2>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path={'/'} element={<Navigate to={'/contacts/list'} />} />
+        <Route path={'/contacts/list'} element={<ContactList />} />
+        <Route path={'/contacts/view/:contactId'} element={<ViewContact />} />
+        <Route path={'/contacts/edit/:contactId'} element={<EditContact />} />
+      </Routes>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 // connecting the bootstrap js file
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
