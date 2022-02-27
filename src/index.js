7ca6e9e96dc1ef3dc9dca3a1076e35e90680f30a
Beattris <beatriceebirim@gmail.com>
2022-02-27 13:20:28 +0100

    connected fontawesome and bootrap to index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// connecting font-awesome icon to the index.js file
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+// connecting the bootstrap css 
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+// connecting the bootstrap js file
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
 
 ReactDOM.render(
   <React.StrictMode>
